5232513590a213f2cbb6bea0f5d3fe35c306497c
juno <geojollyc@gmail.com>
2023-02-04 15:26:18 +0530

    Add codes for DCGAN

---
+++ gan_zoo/dcgan.py
@@ -0,0 +1,85 @@
+from torch import nn
+
+class Generator(nn.Module):
+    def __init__(
+        self, 
+        ngpu,
+        nz,
+        ngf,
+        nc
+    ):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.block1 = nn.Sequential(
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True)
+        )
+        self.block2 = nn.Sequential(
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+        )
+        self.block3 = nn.Sequential(
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+        )
+        self.block4 = nn.Sequential(
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+        )
+        self.block5 = nn.Sequential(
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        return x
+
+class Discriminator(nn.Module):
+    def __init__(
+        self, 
+        ngpu,
+        nc,
+        ndf
+    ):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.block1 = nn.Sequential(
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.block3 = nn.Sequential(
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.block4 = nn.Sequential(
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.block5 = nn.Sequential(
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        return x
